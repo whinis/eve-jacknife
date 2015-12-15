11285fad89bc8923d6b52cdcf865d981fea025dc
Whinis <whinis@whinis.com>
2015-12-14 22:55:34 -0500

    change to get github working

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 // Foundation, Inc.,59 Temple Place,Suite 330,Boston,MA  02111-1307  USA
 // 
 // ****************************************************************************
-// main login code
+// main login code 
 require_once("eve.php");
 if(isset($infoBarFunctions)){
 	$infoBarFunctions[]="get_loginbar";
