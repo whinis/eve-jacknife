41b3c84dfbd2bcdd71ae44a4b70955aadd7c05bf
carlos morgado <chbm@primatas.org>
2016-06-17 23:54:50 +0100

    Add docker single container Dockerfile
    This Dockerfile produces a single container based on php5-apache with
    mysql added and jackknife configured and ready to go.
    
    Build as
        docker build -t jack .
    
    Run as
        docker run -d -p 8888:80  jack
    
    Jackknife will be available under http://localhost:8888/

---
+++ base.config.php
@@ -0,0 +1,65 @@
+<?php
+// ****************************************************************************
+//
+// ZZJ Audit Tool v1.5
+// Copyright (C) 2010  ZigZagJoe (zigzagjoe@gmail.com) and
+// Copyright (C) 2012  Equto   (whinis@whinis.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License,or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+
+
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc.,59 Temple Place,Suite 330,Boston,MA  02111-1307  USA
+//
+// ****************************************************************************
+
+// config file
+
+$sql = "localhost";
+$sql_u = "root";
+$sql_p = "local";
+$db ="jackknife";
+$sql_port="3306";
+
+define("DB_PREFIX","");
+
+//Email Settings
+ini_set("sendmail_from", "");
+ini_set("SMTP", "127.0.0.1");
+ini_set("smtp_port", "25");
+
+
+// Login Settings
+$secureTimeout = "300";
+$salt = "7NVtj3fiaEWS7yv";
+
+date_default_timezone_set ("UTC");
+
+define("API_TABLE","api_lookup");
+define("FITTINGS_TABLE","fit_lookup");
+define("CACHE_TABLE","api_cache");
+define("ID_CACHE_TABLE","id_cache");
+define("TYPE_CACHE_TABLE", "api_type_cache");
+define("CONTRACT_BIDS_TABLE", "contract_bids");
+define("CONTRACT_CONTENTS_TABLE", "contract_items");
+
+
+define("allow_login",false);
+
+
+define("API_BASE_URL","https://api.eveonline.com");
+define("DEBUG",false);
+
+
+
+ ?>
